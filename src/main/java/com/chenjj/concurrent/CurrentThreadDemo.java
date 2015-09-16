dd1605987bc8717a518bcfae6bddffff6eca3a7a
chenjunjiang <chenjunjiang1988@126.com>
2015-09-16 23:41:58 +0800

    第一个例子

---
+++ src/main/java/com/chenjj/concurrent/CurrentThreadDemo.java
@@ -0,0 +1,20 @@
+package com.chenjj.concurrent;
+
+public class CurrentThreadDemo {
+
+	public static void main(String[] args) {
+		Thread currentThread = Thread.currentThread();
+		System.out.println("Current thread：" + currentThread);
+		currentThread.setName("My Thread");
+		System.out.println("After name change：" + currentThread);
+
+		try {
+			for (int i = 5; i > 0; i--) {
+				System.out.println(i);
+				Thread.sleep(1000);
+			}
+		} catch (InterruptedException e) {
+			System.out.println("Main thread interrupted");
+		}
+	}
+}
